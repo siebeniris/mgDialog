8777836aec92c68b372e8982fad87056ed455971
yiyi chen <chen.yiyi@protonmail.com>
2023-03-23 11:12:10 +0100

    script for build data for topic modeling in the server

---
+++ 02_build_data_for_topicModeling.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#SBATCH --partition=prioritized
+#SBATCH --job-name=preprocess
+#SBATCH --output=%j.out
+#SBATCH --time=30:00:00
+#SBATCH --mem=256GB
+
+
+query=$1
+lang=$2
+
+source $HOME/.bashrc
+conda activate mg
+
+cd $HOME/mgDialog
+
+python src/topicModeling/build_data.py "$lang" "$query"
