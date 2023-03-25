eed5f6018ec2c3424e646d31eccea80e63aaca40
yiyi chen <chen.yiyi@protonmail.com>
2023-03-25 16:02:57 +0100

    scripts

--- 07_run_topic_modeling_year.sh
+++ 07_run_topic_modeling_year.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #SBATCH --partition=prioritized
-#SBATCH --job-name=etm
+#SBATCH --job-name=etm_year
 #SBATCH --output=%j.out
 #SBATCH --time=30:00:00
 #SBATCH --mem=256GB
@@ -11,6 +11,7 @@ pageType=$1
 lang=$2
 query=$3
 num_topics=$4
+year=$5
 
 
 source $HOME/.bashrc
@@ -20,4 +21,4 @@ cd $HOME/mgDialog
 
 #python src/topicModeling/skipgram.py --pageType "$pageType" --lang "$lang" --query "$query"
 
-python -m src.topicModeling.main --pageType "$pageType" --lang "$lang" --query "$query"  --num_topics "$num_topics" --epochs 200
+python -m src.topicModeling.main --pageType "$pageType" --lang "$lang" --query "$query"  --num_topics "$num_topics" --year "$year" --epochs 200
