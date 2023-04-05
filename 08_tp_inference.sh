cc4b085e6511c825c45dbab3eb185aa7da566ed6
yiyi chen <chen.yiyi@protonmail.com>
2023-04-05 13:53:51 +0200

    gitignore

---
+++ 08_tp_inference.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#SBATCH --partition=prioritized
+#SBATCH --job-name=etm_year
+#SBATCH --output=%j.out
+#SBATCH --time=30:00:00
+#SBATCH --mem=128GB
+
+
+pageType=$1
+lang=$2
+query=$3
+num_topics=$4
+year=$5
+month=$6
+
+source $HOME/.bashrc
+conda activate mg
+
+cd $HOME/mgDialog
+
+#python src/topicModeling/skipgram.py --pageType "$pageType" --lang "$lang" --query "$query"
+
+python -m src.topicModeling.main --pageType "$pageType" --lang "$lang" --query "$query"  --num_topics "$num_topics" --year "$year" --month "$month" --epochs 200
